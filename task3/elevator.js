d26a73352dd46fe08d45a67ab413f25224cea07f
Anish Ansari <anishansari489@gmail.com>
2022-05-01 23:23:46 +0545

    fixed spelling mistakes

--- task3/elevator.js
+++ task3/elevator.js
@@ -1,30 +1,30 @@
-// function minTime(n, m, [elevatorSpeed, topen, tclose, jhonSpeed]) {
+// function minTime(n, m, [elevatorSpeed, topen, tclose, johnSpeed]) {
 
-//     return (Math.abs(m - n) * Math.min(elevatorSpeed, jhonSpeed) + (m - n !== 0) ? 2 * topen + tclose : 0);
+//     return (Math.abs(m - n) * Math.min(elevatorSpeed, johnSpeed) + (m - n !== 0) ? 2 * topen + tclose : 0);
 // } //doesn't work
 
-function minTime(n, m, [elevatorSpeed, topen, tclose, jhonSpeed]) {
+function minTime(n, m, [elevatorSpeed, topen, tclose, johnSpeed]) {
   if (m > n) {
     // option1: walk to m floor, take elevator from there
     // option2: walk from n to 0
     // wait for elevator to come down to n & take it from n=>0
     return Math.min(
-      (m - n) * jhonSpeed + m * elevatorSpeed + 2 * topen + tclose,
-      n * jhonSpeed,
+      (m - n) * johnSpeed + m * elevatorSpeed + 2 * topen + tclose,
+      n * johnSpeed,
       m * elevatorSpeed + 2 * topen + tclose
     );
   } else if (m === n) {
     //option1: take elevator
     // option2: on foot
-    return Math.min(n * elevatorSpeed + 2 * topen + tclose, n * jhonSpeed);
+    return Math.min(n * elevatorSpeed + 2 * topen + tclose, n * johnSpeed);
   } else {
     // option1: walk down to m and take elev from m=>0
     // option2: walk from n=>0
     // option3: elev from m=>n && n=>0
     return Math.min(
-      (n - m) * jhonSpeed + m * elevatorSpeed + 2 * topen + tclose,
-      n * jhonSpeed,
-      (2 * n - m) * jhonSpeed + m * elevatorSpeed + 2 * topen + tclose
+      (n - m) * johnSpeed + m * elevatorSpeed + 2 * topen + tclose,
+      n * johnSpeed,
+      (2 * n - m) * johnSpeed + m * elevatorSpeed + 2 * topen + tclose
     );
   }
 }
